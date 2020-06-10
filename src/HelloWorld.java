24c3ec25270164afcb2281b215b70a62d9c94f8d
mokong03 <mark.limon03@gmail.com>
2020-06-10 13:41:14 -0700

    initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+
+/**
+ * @author markl
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hello world");
+
+	}
+
+}
